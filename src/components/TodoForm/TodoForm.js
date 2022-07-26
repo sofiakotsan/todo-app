06f23ff2482dcc16fb871c524bceba89e27b5efc
sofiakotsan <sofiakotsan9@gmail.com>
2022-07-25 22:18:06 -0400

    styled todo form

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,12 +1,15 @@
 import React from "react";
+import './TodoForm.css';
 import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
 
 const TodoForm = () => {
     return (
-        <div>
-            <Input placeholder="What do you want to do?"/>
-            <Button>Add Task</Button>
+        <div className="todo-form-wrap">
+            <form action="#" className="todo-form">
+                <Input placeholder="What do you want to do?"/>
+                <Button type="submit">Add Task</Button>
+            </form>
         </div>
     );
 }
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 const Button = ({children, style, ...props}) => {
     let  buttonStyle = style ? 'button--' + style : 'button--filled';
     return (
-        <button className={`button ${buttonStyle}`}>
+        <button className={`button ${buttonStyle}`} {...props}>
             <span>
                 {children}
             </span>
