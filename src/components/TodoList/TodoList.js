197458f0fbc308586141a66cbf4e7798599f8653
sofiakotsan <sofiakotsan9@gmail.com>
2022-07-28 21:12:15 -0400

    added list controls buttons

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,20 +1,28 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import Button from "./../UI/Button/Button";
 import "./TodoList.css"
 
 const TodoList = ({items, deleteItem, toggleIsFinished, ...props}) => {
     return (
         <div className="todo-list-wrap">
             <div className="todo-list">
-                {
-                    items.map(item => {
-                        return  <TodoItem   item={item} 
-                                            key={item.id} 
-                                            deleteItem={deleteItem} 
-                                            toggleIsFinished={toggleIsFinished}/>
-                    })
-                }
+                <div className="todo-list-inner">
+                    {
+                        items.map(item => {
+                            return  <TodoItem   item={item} 
+                                                key={item.id} 
+                                                deleteItem={deleteItem} 
+                                                toggleIsFinished={toggleIsFinished}/>
+                        })
+                    }
+                </div>
             </div>
+            <div className="todo-list-controls">
+                <Button>Finish All</Button>
+                <Button style="outline">Clear</Button>
+            </div>
+            
         </div>
     );
 }
